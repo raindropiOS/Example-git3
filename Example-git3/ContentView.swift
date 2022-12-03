22e14cbd551fba1825f01243a2274d50464c6733
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2022-12-03 21:03:04 +0900

    say hello from sihyeon2

--- Example-git3/ContentView.swift
+++ Example-git3/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("hello git from main")
+            Text("hello git from sihyeon2")
         }
         .padding()
     }
