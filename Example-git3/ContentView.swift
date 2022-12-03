2ba06277f4ef5fc05a72964b8ee8005300074d05
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2022-12-03 21:43:21 +0900

    change say hello

--- Example-git3/ContentView.swift
+++ Example-git3/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "pencil")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world from main!")
             Text("hello git from main")
         }
         .padding()
