7ebd290b33b7ba11d54930813eece0a613cf3d65
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2022-12-03 21:44:57 +0900

    change say hello from sihyeon2

--- Example-git3/ContentView.swift
+++ Example-git3/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "star")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world from sihyeon2!")
             Text("hello git from main")
             Text("hello git from sihyeon2")
             Text("editted from sihyeon2")
