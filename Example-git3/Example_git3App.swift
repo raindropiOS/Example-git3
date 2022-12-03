b95230229520dd493c4783cd4ceb54749aef1b64
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2022-12-03 20:54:52 +0900

    first commit

---
+++ Example-git3/Example_git3App.swift
@@ -0,0 +1,17 @@
+//
+//  Example_git3App.swift
+//  Example-git3
+//
+//  Created by 박시현 on 2022/12/03.
+//
+
+import SwiftUI
+
+@main
+struct Example_git3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
